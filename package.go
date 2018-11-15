80152bc47757aa392359589de998a2873df09a29
wbh1 <wbhegedus@gmail.com>
2018-11-15 17:28:58 -0500

    Fix package info

--- package.go
+++ package.go
@@ -1,20 +1,20 @@
 // Package nagios is a package that is intented to provide a simple, standardized way
 // for Nagios checks to return a result.
-
+//
 // It does so by defining a Result type, which has an exitCode and a message associated with it.
-
+//
 // Implementing this may look like:
 // 		package main
-
+//
 // 		var (
 // 			warning = 80
 // 			critical = 90
 // 			result = nagios.Result{}
 // 		)
-
+//
 // 		func main() {
 // 			diskUsed := checkDiskUsage()
-
+//
 // 			switch {
 // 			case diskUsed > critical:
 // 				result.CRITICAL
@@ -23,7 +23,7 @@
 // 			default:
 // 				result.OK
 // 			}
-
+//
 // 			result.Exit()
 // 		}
 //
